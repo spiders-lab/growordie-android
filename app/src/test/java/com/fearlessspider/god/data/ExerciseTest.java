7930ae5695b784a25646d098a82c67498b890a30
Przemysław Pająk <office@fearlessspider.com>
2022-01-30 20:19:34 +0100

    test(app): add test for Exercise class

---
+++ app/src/test/java/com/fearlessspider/god/data/ExerciseTest.java
@@ -0,0 +1,31 @@
+package com.fearlessspider.god.data;
+
+import com.fearlessspider.god.utilsl;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class ExerciseTest extends Utils {
+    private Exercise exercise;
+
+    @Before
+    public void setUp() {
+        exercise = new Exercise("123", "Comment", "track123");
+    }
+
+    @Test
+    public void testSerializeThenDeserialize() throws IOException, ClassNotFoundException {
+        byte[] serialized1 = serialize(exercise);
+        byte[] serialized2 = serialize(exercise);
+
+        Exercise deserialized1 = (Exercise) deserialize(serialized1);
+        Exercise deserialized2 = (Exercise) deserialize(serialized2);
+
+        Assert.assertEquals(deserialized1.getComment(), deserialized2.getComment());
+        Assert.assertEquals(exercise.getComment(), deserialized1.getComment());
+        Assert.assertEquals(exercise.getComment(), deserialized2.getComment());
+    }
+}
