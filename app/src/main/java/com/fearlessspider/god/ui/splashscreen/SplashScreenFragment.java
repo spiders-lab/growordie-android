faa1c18a4890a2948efb00aef0906e64b43b3071
Przemysław Pająk <office@fearlessspider.com>
2022-02-05 18:11:24 +0100

    feat(app): add ScreenSplash

---
+++ app/src/main/java/com/fearlessspider/god/ui/splashscreen/SplashScreenFragment.java
@@ -0,0 +1,29 @@
+package com.fearlessspider.god.ui.splashscreen;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.FragmentActivity;
+
+import com.fearlessspider.god.MainActivity;
+import com.fearlessspider.god.R;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashScreenFragment extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+        runNextScreen();
+    }
+
+    private void runNextScreen() {
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(SplashScreenFragment.this, MainActivity.class));
+            finish();
+        }, 1500);
+    }
+}
