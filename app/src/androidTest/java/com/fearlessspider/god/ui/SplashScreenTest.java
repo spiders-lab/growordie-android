a5da9e9653f71419698d4a358590b1d304475182
Przemysław Pająk <office@fearlessspider.com>
2022-02-07 14:08:51 +0100

    test(app): update depedencies test(app): update test for SplashScreenTest

--- app/src/androidTest/java/com/fearlessspider/god/ui/SplashScreenTest.java
+++ app/src/androidTest/java/com/fearlessspider/god/ui/SplashScreenTest.java
@@ -1,7 +1,17 @@
 package com.fearlessspider.god.ui;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.fearlessspider.god.ui.splashscreen.SplashScreenFragment;
+
+import org.junit.Rule;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 
 /**
@@ -9,4 +19,16 @@ import org.junit.runner.RunWith;
  */
 @RunWith(AndroidJUnit4.class)
 public class SplashScreenTest {
+    @Rule
+    public ActivityScenarioRule<SplashScreenFragment> activityScenarioRule = new ActivityScenarioRule(SplashScreenFragment.class);
+
+    @Test
+    public void checkTextOnScree() {
+        onView(withText("G.O.D.")).check(matches(isDisplayed()));
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
