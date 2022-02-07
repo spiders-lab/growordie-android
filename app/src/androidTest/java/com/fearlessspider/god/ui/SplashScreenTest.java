6f818b40984400cf32d56f5af6cabfdca52ff665
Przemysław Pająk <office@fearlessspider.com>
2022-02-07 12:25:47 +0100

    test(app): initial SplashScreenTest

---
+++ app/src/androidTest/java/com/fearlessspider/god/ui/SplashScreenTest.java
@@ -0,0 +1,12 @@
+package com.fearlessspider.god.ui;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.runner.RunWith;
+
+/**
+ * Instrumented test, which will test SplashScreen
+ */
+@RunWith(AndroidJUnit4.class)
+public class SplashScreenTest {
+}
