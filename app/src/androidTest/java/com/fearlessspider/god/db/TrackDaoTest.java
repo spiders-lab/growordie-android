fc5a71a9d652a7aec5d32f40e7a2d14ad6bc0303
Przemysław Pająk <office@fearlessspider.com>
2022-02-10 16:56:20 +0100

    test(app): add test for Track

---
+++ app/src/androidTest/java/com/fearlessspider/god/db/TrackDaoTest.java
@@ -0,0 +1,74 @@
+package com.fearlessspider.god.db;
+
+import android.content.Context;
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.fearlessspider.god.LiveDataTestUtil;
+import com.fearlessspider.god.data.Track;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+@RunWith(AndroidJUnit4.class)
+public class TrackDaoTest {
+
+    @Rule
+    public InstantTaskExecutorRule instantTaskExecutorRule = new InstantTaskExecutorRule();
+
+    private TrackDao trackDao;
+    private GODDatabase godDatabase;
+
+    @Before
+    public void createDB() {
+        Context context = ApplicationProvider.getApplicationContext();
+
+        godDatabase = Room.inMemoryDatabaseBuilder(context, GODDatabase.class)
+                .allowMainThreadQueries()
+                .build();
+        trackDao = godDatabase.trackDao();
+    }
+
+    @After
+    public void closeDb() {
+        godDatabase.close();
+    }
+
+    @Test
+    public void insertAndGetTrack() throws Exception {
+        Track track = new Track("Java");
+        trackDao.insert(track);
+        List<Track> allTracks = LiveDataTestUtil.getValue(trackDao.getAlphabetizedTracks());
+        Assert.assertEquals(allTracks.get(0).getName(), track.getName());
+    }
+
+    @Test
+    public void getAllTracks() throws Exception {
+        Track track = new Track("C++");
+        trackDao.insert(track);
+        Track track1 = new Track("Python");
+        trackDao.insert(track1);
+        List<Track> allTracks = LiveDataTestUtil.getValue(trackDao.getAlphabetizedTracks());
+        Assert.assertEquals(allTracks.get(0).getName(), track.getName());
+        Assert.assertEquals(allTracks.get(1).getName(), track1.getName());
+    }
+
+    @Test
+    public void deleteAll() throws Exception {
+        Track track = new Track("JavaScript");
+        trackDao.insert(track);
+        Track track1 = new Track("HTML");
+        trackDao.insert(track1);
+        List<Track> allTracks = LiveDataTestUtil.getValue(trackDao.getAlphabetizedTracks());
+        Assert.assertTrue(allTracks.isEmpty());
+    }
+}
