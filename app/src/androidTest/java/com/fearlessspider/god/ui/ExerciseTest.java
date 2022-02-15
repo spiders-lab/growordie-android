6cccc9f3458a20c041da78b99070b5c3e0ed21c0
Przemysław Pająk <office@fearlessspider.com>
2022-02-15 20:55:01 +0100

    test(app): add toolbar test for Exercise

---
+++ app/src/androidTest/java/com/fearlessspider/god/ui/ExerciseTest.java
@@ -0,0 +1,27 @@
+package com.fearlessspider.god.ui;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+
+import androidx.fragment.app.testing.FragmentScenario;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.fearlessspider.god.R;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class ExerciseTest {
+    @Test
+    public void checkTitleOfScreen() {
+        FragmentScenario.launchInContainer(ExerciseFragment.class);
+
+        onView(withId(R.id.toolbar)).check(matches(isDisplayed()));
+        onView(withText(R.string.exercise)).check(matches(withParent(withId(R.id.toolbar))));
+    }
+}
