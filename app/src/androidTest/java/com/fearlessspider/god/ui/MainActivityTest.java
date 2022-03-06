17c880e77a2ec4028b5f4f6c711b0d3c136fd521
Przemyslaw Pajak <office@fearlessspider.com>
2022-03-06 20:48:53 +0100

    test(app): Added drawer test of main activity

--- app/src/androidTest/java/com/fearlessspider/god/ui/MainActivityTest.java
+++ app/src/androidTest/java/com/fearlessspider/god/ui/MainActivityTest.java
@@ -2,13 +2,19 @@ package com.fearlessspider.god.ui;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
-import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.view.Gravity;
+
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import com.fearlessspider.god.MainActivity;
+import com.fearlessspider.god.R;
 
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,12 +29,17 @@ public class MainActivityTest {
     public ActivityScenarioRule<MainActivity> activityScenarioRule = new ActivityScenarioRule(MainActivity.class);
 
     @Test
-    public void checkTextOnScreen() {
-        onView(withText("G.O.D.")).check(matches(isDisplayed()));
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void clickHomeAndShowsHomeScreen() {
+        // Open Drawer to click on navigation.
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT))) // Left Drawer should be closed.
+                .perform(DrawerActions.open()); // Open Drawer
+
+        // Start the screen of activity.
+        onView(withId(R.id.nav_view))
+                .perform(NavigationViewActions.navigateTo(R.id.nav_home));
+
+        // Check that Activity was opened.
+        onView(withId(R.id.text_home)).check(matches(withText(R.string.steps)));
     }
 }
