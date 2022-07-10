7b72a02df3f4b7a6f1d519d2fe1446e53472cc7c
Przemyslaw Pajak <office@fearlessspider.com>
2022-07-11 00:23:37 +0200

    Added track repository

---
+++ app/src/main/java/com/fearlessspider/god/repository/TrackRepository.java
@@ -0,0 +1,33 @@
+package com.fearlessspider.god.repository;
+
+import android.app.Application;
+
+import androidx.lifecycle.LiveData;
+
+import com.fearlessspider.god.db.GODDatabase;
+import com.fearlessspider.god.db.Journey;
+import com.fearlessspider.god.db.JourneyDao;
+
+import java.util.List;
+
+public class TrackRepository {
+
+    private TrackDao trackDao;
+    private LiveData<List<Track>> trackList;
+
+    public TrackRepository(Application application) {
+        GODDatabase db = GODDatabase.getDatabase(application);
+        trackDao = db.trackDao();
+        trackList = trackDao.getTracks();
+    }
+
+    public LiveData<List<Track>> getTrackList() {
+        return trackList;
+    }
+
+    public void insert(Track track) {
+        GODDatabase.databaseWriteExecutor.execute(() -> {
+            trackDao.insert(track);
+        });
+    }
+}
